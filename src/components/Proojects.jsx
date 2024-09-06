1521b2b53e74ec895ccad57fd2278a1eec11eca0
Rehan <rehanalire52@gmail.com>
2024-09-06 18:52:48 +0530

    Update on Projects page

--- src/components/Proojects.jsx
+++ src/components/Proojects.jsx
@@ -103,21 +103,21 @@ function Proojects() {
             <div className=''>
               <div className='flex items-center '>
                 <div>
-                  <h1 className='font-extrabold mt-5 text-base lg:text-[22px]'>Space-X Clone</h1>
+                  <h1 className='font-extrabold mt-5 text-base lg:text-[22px]'>Job Portal</h1>
                 </div>
                 <div className='mt-3'>
-                  <div className='border-t ml-3 mr-3  border-[#EBECF3] opacity-40 inline-block w-[40vw] lg:w-[250px]' />
+                  <div className='border-t ml-3 mr-3  border-[#EBECF3] opacity-40 inline-block w-[40vw] lg:w-[310px]' />
                 </div>
                 <div className='mt-3'>
                   <span>
-                    <a href="">
+                    <a href="https://github.com/rehan-adi/Job-Portal-Frontend">
                       <svg className='text-gray-400 hover:text-white ' stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 1024 1024" height="2.4rem" width="1.7rem" xmlns="http://www.w3.org/2000/svg"><path d="M511.6 76.3C264.3 76.2 64 276.4 64 523.5 64 718.9 189.3 885 363.8 946c23.5 5.9 19.9-10.8 19.9-22.2v-77.5c-135.7 15.9-141.2-73.9-150.3-88.9C215 726 171.5 718 184.5 703c30.9-15.9 62.4 4 98.9 57.9 26.4 39.1 77.9 32.5 104 26 5.7-23.5 17.9-44.5 34.7-60.8-140.6-25.2-199.2-111-199.2-213 0-49.5 16.3-95 48.3-131.7-20.4-60.5 1.9-112.3 4.9-120 58.1-5.2 118.5 41.6 123.2 45.3 33-8.9 70.7-13.6 112.9-13.6 42.4 0 80.2 4.9 113.5 13.9 11.3-8.6 67.3-48.8 121.3-43.9 2.9 7.7 24.7 58.3 5.5 118 32.4 36.8 48.9 82.7 48.9 132.3 0 102.2-59 188.1-200 212.9a127.5 127.5 0 0 1 38.1 91v112.5c.8 9 0 17.9 15 17.9 177.1-59.7 304.6-227 304.6-424.1 0-247.2-200.4-447.3-447.5-447.3z"></path></svg>
                     </a>
                   </span>
                 </div>
               </div>
               <div className='mt-3'>
-                <h1 className='text-[#0AE448] text-base'>HTML - CSS - Javascript</h1>
+                <h1 className='text-[#0AE448] text-sm'>React - Tailwind - Typescript - ShadCN - React-hook-form - zod</h1>
               </div>
               <div className='flex mt-3 items-center'>
                 <p className='text-lg text-[#EBECF3] opacity-80  '>This is Clone of Space-X website its a Rocket launch <br className='lg:flex hidden' /> company. This  is made by  Elon Musk and <br className='lg:flex hidden' />  built by his team.</p>
@@ -131,21 +131,21 @@ function Proojects() {
             <div >
               <div className='flex items-center'>
                 <div>
-                  <h1 className='font-extrabold mt-5 text-base lg:text-[22px]'>Adidas E-commerene</h1>
+                  <h1 className='font-extrabold mt-5 text-base lg:text-[22px]'>Blog Application</h1>
                 </div>
                 <div className='mt-3'>
-                  <div className='border-t ml-3 mr-3  border-[#EBECF3] opacity-40 inline-block w-[23vw] lg:w-[185px]'></div>
+                  <div className='border-t ml-3 mr-3  border-[#EBECF3] opacity-40 inline-block w-[23vw] lg:w-[240px]'></div>
                 </div>
                 <div className='mt-3'>
                   <span>
-                    <a href="https://github.com/rehan-adi/Adidas_Clone-">
+                    <a href="https://github.com/rehan-adi/Blog-Frontend">
                       <svg className='text-gray-400 hover:text-white ' stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 1024 1024" height="2.4rem" width="1.7rem" xmlns="http://www.w3.org/2000/svg"><path d="M511.6 76.3C264.3 76.2 64 276.4 64 523.5 64 718.9 189.3 885 363.8 946c23.5 5.9 19.9-10.8 19.9-22.2v-77.5c-135.7 15.9-141.2-73.9-150.3-88.9C215 726 171.5 718 184.5 703c30.9-15.9 62.4 4 98.9 57.9 26.4 39.1 77.9 32.5 104 26 5.7-23.5 17.9-44.5 34.7-60.8-140.6-25.2-199.2-111-199.2-213 0-49.5 16.3-95 48.3-131.7-20.4-60.5 1.9-112.3 4.9-120 58.1-5.2 118.5 41.6 123.2 45.3 33-8.9 70.7-13.6 112.9-13.6 42.4 0 80.2 4.9 113.5 13.9 11.3-8.6 67.3-48.8 121.3-43.9 2.9 7.7 24.7 58.3 5.5 118 32.4 36.8 48.9 82.7 48.9 132.3 0 102.2-59 188.1-200 212.9a127.5 127.5 0 0 1 38.1 91v112.5c.8 9 0 17.9 15 17.9 177.1-59.7 304.6-227 304.6-424.1 0-247.2-200.4-447.3-447.5-447.3z"></path></svg>
                     </a>
                   </span>
                 </div>
               </div>
               <div className='mt-3'>
-                <h1 className='text-[#0AE448] text-base'>HTML - CSS - React - Redux - RTK</h1>
+                <h1 className='text-[#0AE448] text-sm'>React - Tailwind - Javascript - react-router-dom</h1>
               </div>
               <div className='flex mt-3 items-center'>
                 <p className='text-lg text-[#EBECF3] opacity-80  '>A real-time coaching app for students learning to <br className='lg:flex hidden' /> paint. This app is my baby, designed and built on <br className='lg:flex hidden' /> my own</p>
